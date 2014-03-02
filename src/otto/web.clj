f00bec4517b0559a95a7950c6eee533a3d5c7a9c
Sebastian Ohm <ohm.sebastian@gmail.com>
2014-03-02 21:00:54 +0100

    web: add some type annotations

--- src/otto/web.clj
+++ src/otto/web.clj
@@ -1,11 +1,16 @@
 (ns otto.web
-  (:require [compojure.core     :as c]
+  (:require [clojure.core.typed :as t :refer :all]
+            [compojure.core     :as c]
             [ring.util.response :as r]))
 
+(def-alias State (HMap :complete? true :mandatory {:organizations (clojure.lang.IPersistentVector otto.organization.Organization)}))
+
+(ann make-state [(clojure.lang.IPersistentVector otto.organization.Organization) -> State])
 (defn- make-state
   [o]
   {:organizations o})
 
+(ann show-organization [State -> String])
 (defn- show-organization
   [s]
   (let [o (first (:organizations s))]
@@ -14,5 +19,5 @@
 (defn make-handler-fn
   [o]
   (let [s (make-state o)]
-    (c/defroutes main-routes
+    (c/defroutes web-routes
       (c/GET "/" [] (show-organization s)))))
