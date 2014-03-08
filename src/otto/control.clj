d8d5c9065f7f29e005b230640a7e9bb49e213342
Sebastian Ohm <ohm.sebastian@gmail.com>
2014-03-08 20:34:44 +0100

    wip: process fetched repositories

--- src/otto/control.clj
+++ src/otto/control.clj
@@ -2,17 +2,25 @@
   (:require [clojure.core.async :as async :refer [<! alts!! chan go timeout]]
             [otto.github :as github]))
 
-(defn update-repositories
-  [orgs user _repos]
+(defn- update-repository
+  [repos r]
+  (println r)
+  (not (nil? r)))
+
+(defn- update-repositories
+  [orgs user update-fn]
   (let [r (chan)
         t (timeout 1000)]
     (doseq [o orgs]
       (github/fetch-repositories o user r))
-    (println (alts!! [r t]))))
+    (loop []
+      (if (update-fn (first (alts!! [r t])))
+        (recur)))))
 
 (defn periodically-update
   [orgs user interval repos]
-  (go (loop []
-        (<! (timeout interval))
-        (update-repositories orgs user repos)
-        (recur))))
+  (let [f (fn [r] (update-repository repos r))]
+    (go (loop []
+          (<! (timeout interval))
+          (update-repositories orgs user f)
+          (recur)))))
