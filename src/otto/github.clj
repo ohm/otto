f69d5a9e2140b18c2960af8da74bf6f5b329f112
Sebastian Ohm <ohm.sebastian@gmail.com>
2014-03-11 22:03:15 +0100

    build organization repositories url

--- src/otto/github.clj
+++ src/otto/github.clj
@@ -1,8 +1,17 @@
 (ns otto.github
   (:require [clojure.core.async :as async :refer [<! >! go timeout]]))
 
+(defn- api-base-url
+  [path]
+  (format "https://api.github.com/%s" path))
+
+(defn- organization-repositories-url
+  [organization]
+  (api-base-url (format "orgs/%s/repos" (:name organization))))
+
 (defn fetch-repositories
   [organization user ch]
-  (go
-    (<! (timeout (rand-int 100)))
-    (>! ch organization)))
+  (let [url (organization-repositories-url organization)]
+    (go
+      (<! (timeout (rand-int 100)))
+      (>! ch url))))
