fc54cb45b02cb43d0d98fac99182ade20d1b1970
Sebastian Ohm <ohm.sebastian@gmail.com>
2014-04-03 20:18:57 +0200

    use bootstrap styles

--- src/otto/html.clj
+++ src/otto/html.clj
@@ -1,18 +1,14 @@
 (ns otto.html
-  (:require [hiccup.page :refer [html5]]))
+  (:require [hiccup.page :as hiccup]))
 
-(defn- layout
-  [body]
-  (html5 body))
-
-(defn- organization-collection
-  [organizations current]
-  [:ul (map (fn [o]
-              (let [n (:name o)]
-                [:li (if (= o current)
-                       {:class "current"})
-                 [:a {:href (format "/%s" n)} n]]))
-            organizations)])
+(defn- organization-navigation
+  [organizations organization]
+  [:ul {:class "nav nav-tabs"}
+   (map (fn [o]
+          (let [n (:name o)]
+            [:li (if (= o organization)
+                   {:class "active"})
+             [:a {:href (format "/%s" n)} n]])) organizations)])
 
 (defn- format-string
   [string]
@@ -30,22 +26,33 @@
   [repositories]
   (if (empty? repositories)
     [:p "Listing currently unavailable."]
-    [:table
+    [:table {:class "table table-condensed"}
      [:thead [:tr [:th "Name"]
                   [:th "Description"]
                   [:th "Language"]
                   [:th "Date"]]]
      [:tbody (for [[_ repository] repositories]
                [:tr (if (= true (:private repository))
-                     {:class "private"})
+                      {:class "active"})
                [:td [:a {:href (:html_url repository)} (:name repository)]]
                [:td (format-string (:description repository))
+                    "&nbsp;"
                     (if (= true (:fork repository))
-                      [:span {:class "fork"} "fork"])]
+                      [:span {:class "label label-default"} "fork"])]
                [:td (format-string (:language repository))]
-               [:td (format-date (:pushed_at repository))]])]]))
+               [:td {:class "date"}
+                (format-date (:pushed_at repository))]])]]))
 
 (defn organization-view
   [organizations organization repositories]
-  (layout [:body (organization-collection organizations organization)
-                 (repository-collection repositories)]))
+  (hiccup/html5 [:head [:link {:href "/bootstrap.min.css"
+                               :rel  "stylesheet"
+                               :type "text/css"}]
+                       [:link {:href "/otto.css"
+                               :rel  "stylesheet"
+                               :type "text/css"}]]
+                [:body [:div {:class "container-fluid"}
+                        [:div {:class "page-header"}
+                         [:h1 "Repositories"]]
+                        (organization-navigation organizations organization)
+                        (repository-collection repositories)]]))
