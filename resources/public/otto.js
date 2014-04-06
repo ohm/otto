249c8769c418c380abec7b59e3300c166b61b830
Sebastian Ohm <ohm.sebastian@gmail.com>
2014-04-06 10:30:07 +0200

    keep checkbox/search filter in sync

--- resources/public/otto.js
+++ resources/public/otto.js
@@ -1,63 +1,60 @@
 $(function () {
-  /**
-   * Fork/Private/Public filters
-   */
-  $("form#filters").change(function () {
-    var _fork = false,
-        _priv = false,
-        _pub  = false;
+  var update = function () {
+    var fork = false,
+        priv = false,
+        pub  = false,
+        term = $("input#search").val();
 
     $("form#filters input:checked").each(function () {
       switch ($(this).val()) {
-        case "fork"    : _fork = true; break;
-        case "private" : _priv = true; break;
-        case "public"  : _pub  = true; break;
-      };
+        case "fork"    : fork = true; break;
+        case "private" : priv = true; break;
+        case "public"  : pub  = true; break;
+      }
     });
 
-    $("table tbody tr").each(function () {
+    return $("table tbody tr").each(function () {
       var elem = $(this),
           show = true;
 
-      if (!_fork && elem.data("fork")) {
+      if (!fork && elem.data("fork")) {
         show = false;
-      };
+      }
 
-      if (!_priv && elem.data("private")) {
+      if (show && !priv && elem.data("private")) {
         show = false;
-      };
+      }
 
-      if (!_pub && !elem.data("private")) {
+      if (show && !pub && !elem.data("private")) {
         show = false;
+      }
+
+      if (show && (term.length > 0)) {
+        if (!RegExp(term, "i").test(elem.data("search"))) {
+          show = false;
+        }
       }
 
       return show ? elem.show() : elem.hide();
     });
-  });
+  };
+
+  /**
+   * Fork/Private/Public filters
+   */
+  $("form#filters").change(update);
 
   /**
    * Simple search / string matching
    */
-  var _searchTimeout = null;
+  var searchTimeout = null;
 
   $("input#search").keyup(function () {
-    if (_searchTimeout) {
-      clearTimeout(_searchTimeout);
-    };
+    if (searchTimeout !== null) {
+      clearTimeout(searchTimeout);
+    }
 
-    var searchTerm = $(this).val();
-    setTimeout(function () {
-      $("table tbody tr").each(function () {
-        var elem = $(this),
-            show = true;
-
-        if (!RegExp(searchTerm, "i").test(elem.data("search"))) {
-          show = false;
-        }
-
-        return show ? elem.show() : elem.hide();
-      });
-    }, 100);
+    searchTimeout = setTimeout(update, 100);
+    return;
   });
-
 });
